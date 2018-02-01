36950be43064f9778494ac8d6362f2d60d3fb3b5
Sam Baskinger <basking2@yahoo.com>
2018-02-01 14:54:19 -0600

    parser_test.go:Adding escaping unit test.

--- sdsai/itrml/parser_test.go
+++ sdsai/itrml/parser_test.go
@@ -27,30 +27,42 @@ func TestParseAListWithData(t *testing.T) {
 
 	switch l := ParseExpression("[string1 \"string two\" 1l 2d 3]").(type) {
 	case *list.List:
-    f := l.Front()
-    if f.Value.(string) != "string1" {
-      t.Error("Expected string1")
-    }
+		f := l.Front()
+		if f.Value.(string) != "string1" {
+			t.Error("Expected string1")
+		}
 
-    f = f.Next()
-    if f.Value.(string) != "string two" {
-      t.Errorf("Expected string two. Got %s", f.Value)
-    }
+		f = f.Next()
+		if f.Value.(string) != "string two" {
+			t.Errorf("Expected string two. Got %s", f.Value)
+		}
 
-    f = f.Next()
-    if f.Value.(int64) != 1 {
-      t.Errorf("Expected int of 1")
-    }
+		f = f.Next()
+		if f.Value.(int64) != 1 {
+			t.Errorf("Expected int of 1")
+		}
 
-    f = f.Next()
-    if f.Value.(float64) != 2.0 {
-      t.Errorf("Expected int of 2.0.")
-    }
+		f = f.Next()
+		if f.Value.(float64) != 2.0 {
+			t.Errorf("Expected int of 2.0.")
+		}
 
-    f = f.Next()
-    if f.Value.(int64) != 3 {
-      t.Errorf("Expected int of 3.")
-    }
+		f = f.Next()
+		if f.Value.(int64) != 3 {
+			t.Errorf("Expected int of 3.")
+		}
+	default:
+		t.Error("Expected list.")
+	}
+}
+
+func TestQuotes(t *testing.T) {
+	switch l := ParseExpression("[\"He said, \\\\\\\"Hello.\\\"\"]").(type) {
+	case *list.List:
+		f := l.Front()
+		if f.Value.(string) != "He said, \\\"Hello.\"" {
+			t.Error("Expected string1")
+		}
 	default:
 		t.Error("Expected list.")
 	}
