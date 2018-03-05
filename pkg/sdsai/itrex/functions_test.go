8e5aa00bff8e2dd991b42b22e4da9b3e9f4d6a3e
Sam Baskinger <basking2@yahoo.com>
2018-03-04 22:26:13 -0600

    functions_test.go: Add basic test for curry.

--- pkg/sdsai/itrex/functions_test.go
+++ pkg/sdsai/itrex/functions_test.go
@@ -3,6 +3,7 @@ package itrex
 import (
 	"bytes"
 	"github.com/basking2/sdsai-itrex-golang/pkg/sdsai/itrml"
+	"github.com/basking2/sdsai-itrex-golang/pkg/sdsai/iterator"
 	"testing"
 )
 
@@ -58,3 +59,33 @@ func TestIfFunction(t *testing.T) {
 		t.Errorf("%d != 4", r.(int64))
 	}
 }
+
+func TestCurryFunction(t *testing.T) {
+	e := NewEvaluator()
+	e.Register("sum", NewBoundFunction(func (i iterator.Iterator, ctx *Context, cbdata interface{}) interface{} {
+		s := int32(0)
+		for i.HasNext() {
+			s += i.Next().(int32)
+		}
+		return s
+		}, nil))
+
+	expr, err := itrml.ParseExpression(`[last
+		[set f [curry sum [int 3] ] ]
+		[[get f] [int 4] [int 6]]
+		]
+		`)
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+	v := e.Evaluate(expr, e.RootContext).(int32)
+	if v != 13 {
+		t.Errorf("Expected 13 but got %d.", v)
+	}
+}
+
+// +map+:: Function.
+// +function+:: Create a function.
+// +fn+:: Fetch or define a function.
+// +curry+:: Curry a function.
